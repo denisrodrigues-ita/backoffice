42d2dbf81c799e7cb62f33537b40dd2d395fad9d
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-10-17 19:32:21 -0300

    feat/dark-mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,22 @@ const config: Config = {
         "2xl": "6rem",
       },
     },
+    extend: {
+      colors: {
+        "gold-dark": "#eec643",
+        "black-dark": "#141414",
+        "gray-dark": "#eef0f2",
+        "blue-dark-50": "#0d21a1",
+        "blue-dark-100": "#011638",
+        "gray-light": "#30343f",
+        "white-light": "#fafaff",
+        "violet-light": "#e4d9ff",
+        "blue-light-50": "#273469",
+        "blue-light-100": "#1e2749",
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
