5399750d2a7db8dabbd1f217eff8a89cfcb371d0
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-11-06 18:26:31 -0300

    update/toast

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from "./Card";
 export { default as NoData } from "./NoData";
 export {default as Dropdown} from "./Dropdown";
 export {default as CustomInput} from "./CustomInput";
+export {default as Toast} from "./Toast";
