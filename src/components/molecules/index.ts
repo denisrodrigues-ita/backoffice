2d47528f679bf945eabb1357c8e702f2a7a99ed0
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-01-09 20:27:34 -0300

    removing customInput

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,5 @@
 export { default as Card } from "./Card";
 export { default as NoData } from "./NoData";
 export { default as Dropdown } from "./Dropdown";
-// export { default as CustomInput } from "./CustomInput";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as Toast } from "./Toast";
