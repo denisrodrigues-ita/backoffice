6a1c349f34ec145e7e7b2b1942029ff7e283d2f3
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-01-09 20:24:55 -0300

    remove customInput

--- src/components/molecules/CustomInput/index.tsx
+++ src/components/molecules/CustomInput/index.tsx
@@ -1,39 +1,40 @@
-import React from "react";
-import { CustomInputProps } from "@/interfaces";
-import { Input, Label } from "@/components/atoms";
+// import React from "react";
+// import { CustomInputProps } from "@/interfaces";
+// import { Input, Label } from "@/components/atoms";
 
-const CustomInput: React.FC<CustomInputProps> = ({
-  type,
-  value,
-  onChange,
-  placeholder,
-  children,
-  onClick,
-  styleProps,
-  stylePropsInput,
-}) => {
-  return (
-    <div
-      className={`print:hidden flex justify-between items-center 
-    border-2 border-blue-light-50 dark:border-gray-dark rounded-lg w-full ${
-      styleProps && styleProps
-    }`}
-    >
-      <Label>
-        <Input
-          type={type}
-          value={value}
-          onChange={(e) => onChange(e)}
-          placeholder={placeholder}
-          onClick={() => {
-            onClick && onClick();
-          }}
-          stylePropsInput={stylePropsInput}
-        />
-      </Label>
-      {children}
-    </div>
-  );
-};
+// const CustomInput: React.FC<CustomInputProps> = ({
+//   type,
+//   value,
+//   onChange,
+//   placeholder,
+//   children,
+//   onClick,
+//   styleProps,
+//   stylePropsInput,
+// }) => {
+//   return (
+//     <div
+//       className={`print:hidden flex justify-between items-center 
+//     border-2 border-blue-light-50 dark:border-gray-dark rounded-lg w-full ${
+//       styleProps && styleProps
+//     }`}
+//     >
+//       <Label>
+//         <Input
+//           type={type}
+//           value={value}
+//           onChange={(e) => onChange(e)}
+//           placeholder={placeholder}
+//           variant="search"
+//           onClick={() => {
+//             onClick && onClick();
+//           }}
+//           stylePropsInput={stylePropsInput}
+//         />
+//       </Label>
+//       {children}
+//     </div>
+//   );
+// };
 
-export default CustomInput;
+// export default CustomInput;
