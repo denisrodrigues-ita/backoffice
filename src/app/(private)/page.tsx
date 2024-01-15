89e2caf768fa432d0b814ca40aa444dbcdbdfef2
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-01-15 19:09:09 -0300

    update types

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -17,6 +17,7 @@ const Home = () => {
   const [isLoading, setIsLoading] = React.useState(false);
   const [dataGuests, setDataGuests] = React.useState<any>();
   const [propsModal, setPropsModal] = React.useState({
+    guestId: -1,
     isOpenModal: false,
     guestName: "",
     attendanceStatus: false,
@@ -46,7 +47,7 @@ const Home = () => {
       }
     };
     fetchGuests();
-  }, []);
+  });
 
   const handlePrint = () => {
     window.print();
